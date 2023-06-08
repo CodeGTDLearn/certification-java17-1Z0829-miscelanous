98d6be6fbbe73e061ee56fb6cbc42c4710d111ce
PauloPortofolio <pvba02@gmail.com>
2023-06-08 13:23:00 -0400

    9.2) General Tests
    -> CONCLUDED:
    - Generic RAW types
    
    -> WORKING:
    
    -> NEXT STEPS:
    
    -> FUTURE IMPLEMENTATIONS:

--- src/main/java/org/example/generics/MyRawGenericClass.java
+++ src/main/java/org/example/generics/MyRawGenericClass.java
@@ -1,13 +1,14 @@
 package org.example.generics;
 
-import static java.lang.System.*;
+import static java.lang.System.out;
 
 public class MyRawGenericClass<T> {
 
   T myVariable;
 
   public <T> String transform(T t) {
-    return t.toString() + "-" + t.hashCode();
+
+    return t.toString() + "-" + t.hashCode()+ "-" + t.getClass();
   }
 
   public static void main(String[] args) {
@@ -15,10 +16,10 @@ public class MyRawGenericClass<T> {
     MyRawGenericClass gc = new MyRawGenericClass();
     out.println(gc.transform(1));
     out.println(gc.transform("hello"));
-    out.println(gc.myVariable);
-
 
     MyRawGenericClass<String> gcStr = new MyRawGenericClass<String>();
     out.println(gcStr.transform(1.1));
+
+    out.println(gc.myVariable);
   }
 }
